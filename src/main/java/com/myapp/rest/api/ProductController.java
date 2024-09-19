cc8959cf21d9da0dcfe45f1da8078192e22e147c
pradeep <pradeep@gmail.com>
2024-09-19 10:32:33 +0530

    Exception Handlers

--- src/main/java/com/myapp/rest/api/ProductController.java
+++ src/main/java/com/myapp/rest/api/ProductController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myapp.rest.model.Product;
@@ -40,35 +43,69 @@ public class ProductController {
 	//http://IP:8080/api/v1/products
 	@PostMapping
 	public Product createProduct(@RequestBody Product product){
+		
+		if(map.containsKey(product.productId())) {
+			throw new ProductAlreadyExistsException("Product Already Exists");
+		}
 		return map.putIfAbsent(product.productId(), product);
 	}
 	
 	//http://IP:8080/api/v1/products/Iphone16
 	@GetMapping("/{name}")
 	public List<Product> listProductsByName(@PathVariable("name") String productName){
-		return map.values().stream().filter(p -> p.productName().equalsIgnoreCase(productName))
+		List<Product> products = map.values().stream().filter(p -> p.productName().equalsIgnoreCase(productName))
 				.collect(Collectors.toList());
+		if(products.size() ==0) {
+			throw new ProductNotFoundException("Product With Name "+productName+" Does not exist");
+		}
+		return products;
 	}
 	
-	//http://IP:8080/api/v1/products/Iphone16
+	//http://IP:8080/api/v1/products/1
 		@DeleteMapping("/{id}")
 		public String deleteProduct(@PathVariable("id") Integer productId){
+			if(map.containsKey(productId)) {
+				throw new ProductNotFoundException("Product With Name "+productId+" Does not exist");
+			}
 			
 			Product product = map.remove(productId);
 			return product.productName() + " deleted";
 		}
 		
 
-		//http://IP:8080/api/v1/products
+		//http://IP:8080/api/v1/products 
+		// httpbody {"productName":"","price":78787,"rating":}
 		@PutMapping("/{id}")
 		public Product updateProduct(@PathVariable("id") Integer productId, @RequestBody Product product){
 			
 			return map.computeIfPresent(productId, (Integer key,Product oldProduct) -> product);
 		}	
 		
+		@ResponseStatus(value = HttpStatus.NOT_FOUND,reason="Not Found")
+		@ExceptionHandler(ProductNotFoundException.class)
+		public void notFoundHandler() {
+			
+		}
+		
+		
+		@ResponseStatus(value = HttpStatus.NOT_FOUND,reason="Already Exists")
+		@ExceptionHandler(ProductAlreadyExistsException.class)
+		public void alreadyExistsHandler() {
+			
+		}
+		
+		
 	
+	// @PathVariable
+	// @RequestBody
+	// @RequestHeader
+	// @RequestParam	
 	
-	
+		// GET
+		// GET with an identifier
+		// POST creates a new resource
+		// PUT updates a resource
+		// DELETE delete a resource
 	
 	
 
